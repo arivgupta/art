9a0e2ce4d167309588d46a1ef658f60b4e882dd3
Ami Onodera <onodera.camila@gmail.com>
2020-11-09 16:42:01 +0100

    wip

--- plant-life/menu.js
+++ plant-life/menu.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const toggleTag = document.querySelector("a.toggle-nav");
+const mainTag = document.querySelector("main");
+
+toggleTag.addEventListener("click", function() {
+    mainTag.classList.toggle("open")
+
+    if(mainTag.classList.contains("open")) {
+        toggleTag.innerHTML = `<img src="/assets/close.svg"> Close`
+    } else {
+        toggleTag.innerHTML = `<img src="/assets/menu.svg"> Menu`
+    }
+})
